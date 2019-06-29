845687078e9add4519e74890fb93ea9ff22a1f57
WaterKat <waterkatgames@gmail.com>
2019-06-29 15:32:25 -0700

    PlayerUpdate

---
+++ TiltedGameJam/Assets/Scenes/WaterKat/Prefabs/Player/Scripts/PlayerHealth.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerHealth : MonoBehaviour
+{
+    [SerializeField]
+    private int health;
+    [SerializeField]
+    private int maxHealth;
+
+
+    public void Damage(int amount)
+    {
+        if (amount == 0) { amount = 10; }
+        health -= amount;
+        if (amount <= 0)
+        {
+            Die();
+        }
+    }
+
+    public void Heal(int amount)
+    {
+        health += amount;
+        health = (int)Mathf.Min(health, maxHealth);
+    }
+
+    [ContextMenu("DealDamage")]
+    void Die()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+}
---
+++ TiltedGameJam/Assets/Scenes/WaterKat/Scripts/Interfaces.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace WaterKat
+{
+    public class WKEntity
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
