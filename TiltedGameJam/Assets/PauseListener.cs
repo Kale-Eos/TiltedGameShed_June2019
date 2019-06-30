5c4594a87885a5e41f2823e4a262442d7c963520
matt19271 <matthew.j.berrios@gmail.com>
2019-06-29 18:45:14 -0700

    Added pause menu prefab

--- TiltedGameJam/Assets/PauseListener.cs
+++ TiltedGameJam/Assets/PauseListener.cs
@@ -14,7 +14,7 @@ public class PauseListener : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.P))
+        if(Input.GetKeyDown(KeyCode.Escape))
         {
             PauseGame();
         }
